674d537388ed5a14a771ccb07b22657d77c9b737
Fairy <fairy_th@foxmail.com>
2025-10-23 23:59:57 +0800

    1023.2

--- m_AFDM_BPSK_ML_.m
+++ m_AFDM_BPSK_ML_.m
@@ -11,11 +11,11 @@ delta_f = 15e3;  % symbol spacing    符号间距
 T = 1/delta_f;      % symbol duration   符号持续时间
 
 eng_sqrt = (M_mod==2)+(M_mod~=2)*sqrt((M_mod-1)/6*(2^2));   % average power per symbol
-SNR_dB = 0:2:16;    % set SNR here
+SNR_dB = 0:2:10;    % set SNR here
 SNR = 10.^(SNR_dB/10);
 sigma_2 = (abs(eng_sqrt)^2)./SNR;   % noise power
 
-N_frame = 100000;    % number of simulation frames
+N_frame = 10000;    % number of simulation frames
 
 %% Generate synthetic delay-Doppler channel %% 生成合成延迟-多普勒信道
 k_max = 1;      % maximum normalized Doppler index
