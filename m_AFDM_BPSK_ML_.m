f19ed1cd6ae8b6d45c14de74d4961983e1358f17
Fairy <fairy_th@foxmail.com>
2025-10-24 15:51:12 +0800

    Update m_AFDM_BPSK_ML_.m

--- m_AFDM_BPSK_ML_.m
+++ m_AFDM_BPSK_ML_.m
@@ -15,25 +15,28 @@ SNR_dB = 0:2:20;    % set SNR here
 SNR = 10.^(SNR_dB/10);
 sigma_2 = (abs(eng_sqrt)^2)./SNR;   % noise power
 
-N_frame = 100000;    % number of simulation frames
+N_frame = 10000;    % number of simulation frames
 
 %% Generate synthetic delay-Doppler channel %% 生成合成延迟-多普勒信道
-k_max = 1;      % maximum normalized Doppler index
 
 figure();
-for delay_index = 1:3
-    %% Start main simulation %%
+for num_of_Path = 2:4
+
     ber = zeros(size(SNR_dB));
+
+    k_max = 1;      % maximum normalized Doppler index
+
+
     for iesn0 = 1:length(SNR_dB)
 
         sigma2 = sigma_2(iesn0);
-        err_count = zeros(size(SNR_dB));
+        err_count = zeros(size(N_frame));
 
         for iframe = 1:N_frame
 
             %% Generate synthetic delay-Doppler channel %% 生成合成延迟-多普勒信道
-            taps = delay_index+1;       % number of paths
-            l_max = delay_index;      % maximum normalized delay index
+            taps = num_of_Path + 1;       % number of paths
+            l_max = num_of_Path;      % maximum normalized delay index
             % k_max = 1;      % maximum normalized Doppler index
             chan_coef = 1/sqrt(2*taps).*(randn(1,taps)+1i.*randn(1,taps));   % follows Rayleigh distribution
 
@@ -119,12 +122,14 @@ for delay_index = 1:3
         end
         ber(iesn0) = sum(err_count)/length(x)/N_frame;  % calculate bit error rate
     end
+    
     disp(ber)
 
     %% Plot bit error rate %%
-
+    fprintf("P=%d, ", num_of_Path);
     semilogy(SNR_dB, ber)
     hold on
+
 end
 hold off
 
